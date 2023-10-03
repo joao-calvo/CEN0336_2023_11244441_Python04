e12044fcb49ea580b63490037244202d8b131a0c
joao-calvo <112580072+joao-calvo@users.noreply.github.com>
2023-10-03 12:33:45 -0300

    Add files via upload

---
+++ Python04_6.1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Iterate through each element of this list using a for loop: [101,2,15,22,95,33,2,27,72,15,52].
+
+lista = [101,2,15,22,95,33,2,27,72,15,52]
+
+for i in lista:
+  print (i)
+
+
---
+++ Python04_6.2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Print out only the values that are even (hint: use the modulus operator).
+
+lista = [101,2,15,22,95,33,2,27,72,15,52]
+for numero in lista:
+  if (numero % 2 == 0):
+   print (numero)
