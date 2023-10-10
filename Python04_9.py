053f205baaf8d70ed55f22115caacd9f455feffc
joao-calvo <112580072+joao-calvo@users.noreply.github.com>
2023-10-09 22:08:04 -0300

    Update Python04_9.py

--- Python04_9.py
+++ Python04_9.py
@@ -3,6 +3,10 @@
 # Create a new script that uses list comprehension to do the same thing as problem 8. (Use range() to print out every number between 1 and 100.
 
 lista_numeros = [numero for numero in range(1,101)]
-print (lista_numeros)
+
+for i in lista_numeros:
+ print (i)  #imprimindo elemento por elemento
+
+print ("Impressão da lista completa:" , lista_numeros) #imprimindo a lista completa
 
 
